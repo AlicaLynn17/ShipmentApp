932d90cb6e70152506764bb6db125b81405e80d0
AlicaLynn17 <alicalynntabada@gmail.com>
2025-04-30 18:30:39 +0800

    tell me why
    tell me why

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home/home.page'; // Import standalone component
-import { Index2Page } from './index2/index2.page'; // Import standalone component
 
 const routes: Routes = [
   { path: '', component: HomePage }, // Default route for index.html
-  { path: 'index2', component: Index2Page }, // Route for index2.html
 ];
 
 @NgModule({
